37c22b93c0a6a5c093f1aa99c0951d92be027af7
va tri <vatriang@gmail.com>
2025-11-21 11:07:19 +0700

    Menambahkan index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Belajar Git Remote</title>
+</head>
+<body>
+    <h2>Selamat Datang!</h2>
+    <p>Ini adalah contoh halaman sederhana untuk latihan menggunakan Git dan GitHub.</p>
+</body>
+</html>
