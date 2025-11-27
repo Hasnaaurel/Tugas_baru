cdd72a43001c4ae1d015a131fedf34946a26ffc6
Hasnaaurel <hasnawidasari15@gmail.com>
2025-11-27 15:06:19 +0700

    Update index.php
    mengubah file nya

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Belajar Git Remote</title>
 </head>
 <body>
-    <h2>Selamat Datang!</h2>
+    <h2>Selamat Datang dan selamat sore!</h2>
     <p>Ini adalah contoh halaman sederhana untuk latihan menggunakan Git dan b.</p>
 </body>
 </html>
